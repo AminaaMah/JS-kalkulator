2a8ccfe2c9f1a8b6705f0310246df5ce2c6f21b9
AminaaMah <165832625+AminaaMah@users.noreply.github.com>
2024-04-06 23:32:13 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+function addition(num1,num2){
+  let sum=num1+num2;
+  return sum;
+}
+console.log(addition(2,3));
+
+function subtract(num1,num2){
+  let subRez=broj1-broj2;
+  return subRez;
+}
+console.log(subtract(8,2));
+
+function multiply(broj1,broj2){
+  let mulRez=broj1*broj2;
+  return proizvod;
+}
+console.log(multiply(5,7));
+
+function divide(broj1,broj2){
+  let divRez=broj1/broj2;
+  return kolicnik;
+}
+console.log(divide(10,4));
